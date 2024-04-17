912084ddbdf0da86cd34f9523fd4635c60061e64
Christopher V <ChrisRVill@gmail.com>
2024-04-17 00:07:05 -0700

    at this point Im more lost then found

--- travelnetwork.go
+++ travelnetwork.go
@@ -2,40 +2,41 @@ package main
 
 //Node for a Tree or List
 type City struct {
-	Name        string
-	Connections []Link
+	Name          string
+	NeighborCites []Link
 }
 
 //A linked city
 type Link struct {
 	City     City  //The linked city
-	Cost     int64 //The cost to get from the city to the linked city
 	Distance int64 //The distance between the 2 cities
 }
 
-//Where the trip is starting from, going to, and the path to get there.
-type Network struct {
-	CurrentCity  City
-	TargetCity City
-	Path   []City
-	PathWeight int64
+type Path struct {
+	Current       City
+	Target        City
+	Route         []Link
+	RouteDistance int64
 }
 
-func SetStartCity(city string) Network {
-	startCity := City{Name: city}
-	newNetwork := Network{Start: startCity}
+func FindPath(p Path) Path {
+	currentPath := p
+	currentCity := p.Current
 
-	return newNetwork
-}
-
-func GetCity(network Network) Network {
-	for _, nextCity := range 
-	network.Path = append(network.Path, network.Start)
-	
-}
-
-func SearchForCity(city string, network Network) Network {
-
-
-	return
+	for _, NeighborCity := range currentCity.NeighborCites {
+		if &NeighborCity.City == &currentPath.Target {
+			var distanceFromStart int64
+			for _, link := range currentPath.Route {
+				distanceFromStart += link.Distance
+			}
+			if distanceFromStart < currentPath.RouteDistance {
+				currentPath.RouteDistance = distanceFromStart
+				currentPath.Route = append(currentPath.Route, NeighborCity)
+				return currentPath
+			}
+		} else {
+			currentPath = FindPath(currentPath)
+		}
+	}
+	return currentPath
 }
